d8185d9a3292a59d9dbdcc0d9305ad707c233709
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2022-11-24 11:57:41 +0100

    Improved test install file.

--- python_tutorials/computer_vision/install_test.py
+++ python_tutorials/computer_vision/install_test.py
@@ -1,2 +1,17 @@
-import naoqi
-import cv2
+import sys
+
+if sys.version_info.major != 2:
+    print("Please run with python2.7, not python3")
+    sys.exit(1)
+
+try:
+    import cv2
+    print("OpenCV installed correctly")
+except ImportError:
+    print("OpenCV not installed")
+
+try:
+    import naoqi
+    print("naoqi installed correctly")
+except ImportError:
+    print("naoqi not installed")
